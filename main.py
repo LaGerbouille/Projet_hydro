5c0162063e9961f1c296f428bf9d299273be1a2f
LaGerbouille <gerbaud.lopulou@gmail.com>
2025-05-15 12:16:15 +0200

    courbure

--- main.py
+++ main.py
@@ -127,14 +127,23 @@ class Calcul_attribut (object):
 
     # -------------------------------------------------------- COURBURE --------------------------------------------------------------
 
-    def courbures(self):
-        if self.pente_TPP() == 0:
+    def courbures_TPP(self):
+        fx = (self.mnt[1:-1, 2:] - self.mnt[1:-1, 1:-1]) / self.pas
+        fy = (self.mnt[:-2, 1:-1] - self.mnt[1:-1, 1:-1]) / self.pas
+        z1 = self.mnt[:-2, :-2]
+        z2 = self.mnt[:-2, 1:-1]
+        z3 = self.mnt[:-2, 2:]
+        z4 = self.mnt[1:-1, :-2]
+        z5 = self.mnt[1:-1, 1:-1]
+        z6 = self.mnt[1:-1, 2:]
+        z7 = self.mnt[2:, :-2]
+        z8 = self.mnt[2:, 1:-1]
+        z9 = self.mnt[2:, 2:]
+        if self.pente(fx,fy) == 0:
             kv = np.nan
             kh = np.nan
         else:
-            fx = (self.mnt[1:-1, 2:] - self.mnt[1:-1, 1:-1]) / self.pas
-            fy = (self.mnt[:-2, 1:-1] - self.mnt[1:-1, 1:-1]) / self.pas
-            fxx = (self.mnt[1:-1, 2:] - 2 * self.mnt[1:-1, 1:-1] + self.mnt[1:-1, :-2]) / self.pas ** 2
+            fxx = (z6 - 2 * self.mnt[1:-1, 1:-1] + self.mnt[1:-1, :-2]) / self.pas ** 2
             fyy = (self.mnt[2:, 1:-1] - 2 * self.mnt[1:-1, 1:-1] + self.mnt[:-2, 1:-1]) / self.pas ** 2
             fxy = (self.mnt[:-1, 1:] - self.mnt[:-1, :-1] - self.mnt[1:, 1:] + self.mnt[1:, :-1]) / (self.pas ** 2)
             p = fx ** 2 + fy ** 2
@@ -142,6 +151,32 @@ class Calcul_attribut (object):
             kv = -(fxx * fx ** 2 + 2 * fxy * fx * fy + fyy * fy ** 2) / (p * (q ** 3) ** (1 / 2))
             kh = -(fxx * fy ** 2 - 2 * fxy * fx * fy + fyy * fx ** 2) / (p * q ** (1 / 2))
 
+        return kv, kh
+
+    def courbures_FCN(self):
+        fx = (self.mnt[1:-1, 2:] - self.mnt[1:-1, :-2]) / (2 * self.pas)
+        fy = (self.mnt[:-2, 1:-1] - self.mnt[2:, 1:-1]) / (2 * self.pas)
+        z1 = self.mnt[:-2, :-2]
+        z2 = self.mnt[:-2, 1:-1]
+        z3 = self.mnt[:-2, 2:]
+        z4 = self.mnt[1:-1, :-2]
+        z5 = self.mnt[1:-1, 1:-1]
+        z6 = self.mnt[1:-1, 2:]
+        z7 = self.mnt[2:, :-2]
+        z8 = self.mnt[2:, 1:-1]
+        z9 = self.mnt[2:, 2:]
+        delta_x = delta_y = self.pas
+        if self.pente(fx,fy) == 0:
+            kv = np.nan
+            kh = np.nan
+        else:
+            fxx = (z6 - 2 * z5 + z4) / (delta_x ** 2)
+            fyy = (z2 - 2 * z5 + z8) / (delta_y ** 2)
+            fxy = (z1 - z3 - z7 + z9) / (4 * delta_x * delta_y)
+            p = fx ** 2 + fy ** 2
+            q = p + 1
+            kv = -(fxx * fx ** 2 + 2 * fxy * fx * fy + fyy * fy ** 2) / (p * (q ** 3) ** (1 / 2))
+            kh = -(fxx * fy ** 2 - 2 * fxy * fx * fy + fyy * fx ** 2) / (p * q ** (1 / 2))
         return kv, kh
 
     def courbures_approxi(self):
