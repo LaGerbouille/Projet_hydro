39cda807cb7ee98a60a928ed416dc6ceb45a3d3c
Ilyann Aragon <ilyannaragon@gmail.com>
2025-05-23 17:38:26 +0200

    rugosite, BPI

--- BPI.py
+++ BPI.py
@@ -10,53 +10,49 @@ class BPI():
         self.pas = pas
         self.name = name
     def carre(self,n):
-        carre=np.ones(n)
-        carre[n/2][n/2]=0
-        carre = carre/(n**2-1)
-        return carre
+        if n % 2 == 0:
+            raise ValueError("La taille du noyau doit être impair")
+        noyau=np.ones((n,n))
+        centre = n //2
+        noyau[centre,centre] = 0
+        noyau = noyau/(n**2-1)
+        return noyau
     
     def cercle(self,r):
-        x = np.arange(-r,r+1,1)
-        y = np.arange(-r,r+1,1)
-        X,Y = np.meshgrid(x,y)
-        D = (X**2 + Y**2)**0.5
-        M = (D<=r)*1
-        return M
+        x = np.arange(-r, r + 1)
+        y = np.arange(-r, r + 1)
+        X, Y = np.meshgrid(x, y)
+        D = np.sqrt(X**2 + Y**2)
+        
+        masque = (D <= r).astype(int)
+        centre = r 
+        masque[centre, centre] = 0  
+        masque = masque / np.sum(masque) 
+        return masque
 
-    def bpi_carre(self):
-        noyau = np.array([
-            [1, 1, 1],
-            [1, 0, 1],
-            [1, 1, 1]
-        ]) / 8
+    def bpi_carre(self,n):
+        noyau = self.carre(n)
         voisins = convolve(self.mnt,noyau,mode='constant',cval=np.nan)
         bpi = self.mnt - voisins
         return bpi
 
-    def bpi_cercle(self):
-        noyau = np.array([
-            [0,1,1,1,0],
-            [1,1,1,1,1],
-            [1,1,0,1,1],
-            [1,1,1,1,1],
-            [0,1,1,1,0],
-        ]) / 20
+    def bpi_cercle(self,r):
+        noyau = self.cercle(r)
         voisins = convolve(self.mnt,noyau,mode='constant',cval=np.nan)
         bpi = self.mnt - voisins
         return bpi
 
-    def affichage_bpi_cercle(self):
+    def affichage_bpi_cercle(self,r):
         plt.figure()
-        plt.imshow(self.bpi_cercle(), origin='lower', cmap='bwr',norm=CenteredNorm(0,halfrange=0.1))
+        plt.imshow(self.bpi_cercle(r), origin='lower', cmap='bwr',norm=CenteredNorm(0,halfrange=0.1))
         plt.title(f'BPI cercle de {self.name}')
         plt.colorbar(label='BPI')
         plt.show()
-        print('min:',np.nanmin(self.bpi_cercle()),'max:',np.nanmax(self.bpi_cercle()))
-
-    def affichage_bpi_carre(self):
+        
+    def affichage_bpi_carre(self,n):
 
         plt.figure()
-        plt.imshow(self.bpi_carre(), origin='lower', cmap='bwr',norm=CenteredNorm(0,halfrange=0.1))
+        plt.imshow(self.bpi_carre(n), origin='lower', cmap='bwr',norm=CenteredNorm(0,halfrange=0.1))
         plt.title(f'BPI de {self.name}')
         plt.colorbar(label='BPI')
         plt.show()
@@ -67,6 +63,7 @@ mnt = np.loadtxt("MNT/" + fichier)
 pas = 8
 pas_bpi=1
 name = fichier[:-4]
-
 bpi = BPI(mnt, pas_bpi, name)
+bpi.affichage_bpi_carre(3)
+bpi.affichage_bpi_cercle(3)
 
--- Rugosite.py
+++ Rugosite.py
@@ -1,7 +1,71 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import generic_filter
+from scipy.ndimage import convolve
 
 class Rugosite():
     def __init__(self, mnt, pas, name):
         self.mnt = mnt
         self.pas = pas
         self.name = name
+     # def rug_ecart_type(self, n):
+    #     # à ne pas utiliser pour calculer la rugosité 3*3,5*5,7*7 ... car cette méthode demande bcp trop de temps de calcul
+    #     print('je suis au debut de rug_ecart_type')
+    #     rugosite = generic_filter(self.mnt, np.nanstd, size=n, mode='constant', cval=np.nan)
+    #     print('je suis a la fin de rug_ecart_type')
+    #     return rugosite
+
+    # def rugosite_ecart_type_plot(self, n):
+    #     rugosite = self.rug_ecart_type(n)
+    #     print('je suis dans rug_ecart_type_plot')
+    #     plt.figure()
+    #     plt.imshow(rugosite, origin='lower', cmap='viridis')
+    #     plt.title(f'Rugosité (écart-type) de {self.name}')
+    #     plt.colorbar(label='Rugosité')
+    #     plt.show()
+
+    def rugosite_ecart_type_analytique(self, n):
+
+        if n % 2 == 0:
+            raise ValueError("La taille du noyau doit être impaire")
+        noyau = np.ones((n, n)) / (n**2)
+        
+        mnt_carre = self.mnt ** 2
+        
+        voisins_mnt_carre = convolve(mnt_carre, noyau, mode='constant', cval=np.nan)
+        
+        voisins = convolve(self.mnt, noyau, mode='constant', cval=np.nan)
+        
+        rugosite = np.sqrt(np.clip(voisins_mnt_carre - voisins**2, 0, None)) #le np.clip permet d'éviter les racines carrés de nombres négatifs        
+        return rugosite
+
+    def subplot_rugosite(self, tailles):
+        
+        fig, axes = plt.subplots(3, 3, figsize=(15, 12))
+        
+        fig.suptitle(f'Rugosité (écart-type) pour différents voisinages - {rugosite.name}', fontsize=16)        
+        
+        for ax, n in zip(axes.flat, tailles_voisinage):
+
+            rug = self.rugosite_ecart_type_analytique(n)
+            
+            img = ax.imshow(rug, origin='lower', cmap='viridis')
+            
+            ax.set_title(f'Noyau {n}x{n}')
+            
+            ax.axis('off')
+            
+            fig.colorbar(img, ax=ax, shrink=0.6)
+            
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.90)
+        plt.show()
+
+fichier = 'Dune2_Dunkerque_Extrait1_50cm.xyz' 
+mnt = np.loadtxt("MNT/" + fichier)
+pas = 8
+name = fichier[:-4]
+rugosite = Rugosite(mnt, pas, name)
+
+tailles_voisinage = [3, 5, 7, 9, 11, 13, 15,17, 19]
+rugosite.subplot_rugosite(tailles_voisinage)
