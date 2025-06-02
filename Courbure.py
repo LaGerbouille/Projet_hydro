44f5b0d80373eb1f8ab11f4601ef2c656565064e
LaGerbouille <gerbaud.lopulou@gmail.com>
2025-06-02 14:06:47 +0200

    courburemethode

--- Courbure.py
+++ Courbure.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.ndimage import convolve
+
 
 class Courbure():
     def __init__(self, mnt, pas, name):
@@ -6,76 +8,86 @@ class Courbure():
         self.pas = pas
         self.name = name
 
-    def courbures_TPP(self):
-        fx = (self.mnt[1:-1, 2:] - self.mnt[1:-1, 1:-1]) / self.pas
-        fy = (self.mnt[:-2, 1:-1] - self.mnt[1:-1, 1:-1]) / self.pas
-        z1 = self.mnt[:-2, :-2]
-        z2 = self.mnt[:-2, 1:-1]
-        z3 = self.mnt[:-2, 2:]
-        z4 = self.mnt[1:-1, :-2]
-        z5 = self.mnt[1:-1, 1:-1]
-        z6 = self.mnt[1:-1, 2:]
-        z7 = self.mnt[2:, :-2]
-        z8 = self.mnt[2:, 1:-1]
-        z9 = self.mnt[2:, 2:]
-        if self.pente(fx, fy) == 0:
-            kv = np.nan
-            kh = np.nan
-        else:
-            fxx = (z6 - 2 * self.mnt[1:-1, 1:-1] + self.mnt[1:-1, :-2]) / self.pas ** 2
-            fyy = (self.mnt[2:, 1:-1] - 2 * self.mnt[1:-1, 1:-1] + self.mnt[:-2, 1:-1]) / self.pas ** 2
-            fxy = (self.mnt[:-1, 1:] - self.mnt[:-1, :-1] - self.mnt[1:, 1:] + self.mnt[1:, :-1]) / (self.pas ** 2)
-            p = fx ** 2 + fy ** 2
-            q = p + 1
-            kv = -(fxx * fx ** 2 + 2 * fxy * fx * fy + fyy * fy ** 2) / (p * (q ** 3) ** (1 / 2))
-            kh = -(fxx * fy ** 2 - 2 * fxy * fx * fy + fyy * fx ** 2) / (p * q ** (1 / 2))
-
+    def courbures_pente_non_nulle(self):
+        grilleA = np.array([[1, -2, 1], [1, -2, 1], [1, -2, 1]]) / (6 * self.pas ** 2)
+        A = convolve(self.mnt, grilleA, mode='constant', cval=0.0)
+        grilleB = np.array([[1, 1, 1], [-2, -2, -2], [1, 1, 1]]) / (6 * self.pas ** 2)
+        B = convolve(self.mnt, grilleB, mode='constant', cval=0.0)
+        grilleC = np.array([[-1, 0, 1], [0, 0, 0], [1, 0, -1]]) / (4 * self.pas ** 2)
+        C = convolve(self.mnt, grilleC, mode='constant', cval=0.0)
+        grilleD = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]) / (6 * self.pas ** 2)
+        D = convolve(self.mnt, grilleD, mode='constant', cval=0.0)
+        grilleE = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]]) / (6 * self.pas ** 2)
+        E = convolve(self.mnt, grilleE, mode='constant', cval=0.0)
+        grilleF = np.array([[-1, 2, -1], [2, 5, 2], [-1, 2, -1]]) / 9
+        F = convolve(self.mnt, grilleF, mode='constant', cval=0.0)
+        fx = D
+        fy = E
+        fxx = 2 * A
+        fyy = 2 * B
+        fxy = C
+        eps = 0.01
+        p = (fx ** 2 + fy ** 2) * ((fx ** 2 + fy ** 2) < eps) * np.nan
+        q = p + 1
+        kv = -(fxx * fx ** 2 + 2 * fxy * fx * fy + fyy * fy ** 2) / (p * (q ** 3) ** (1 / 2))
+        kh = -(fxx * fy ** 2 - 2 * fxy * fx * fy + fyy * fx ** 2) / (p * q ** (1 / 2))
         return kv, kh
 
 
-    def courbures_FCN(self):
-        fx = (self.mnt[1:-1, 2:] - self.mnt[1:-1, :-2]) / (2 * self.pas)
-        fy = (self.mnt[:-2, 1:-1] - self.mnt[2:, 1:-1]) / (2 * self.pas)
-        z1 = self.mnt[:-2, :-2]
-        z2 = self.mnt[:-2, 1:-1]
-        z3 = self.mnt[:-2, 2:]
-        z4 = self.mnt[1:-1, :-2]
-        z5 = self.mnt[1:-1, 1:-1]
-        z6 = self.mnt[1:-1, 2:]
-        z7 = self.mnt[2:, :-2]
-        z8 = self.mnt[2:, 1:-1]
-        z9 = self.mnt[2:, 2:]
-        delta_x = delta_y = self.pas
-        if self.pente(fx, fy) == 0:
-            kv = np.nan
-            kh = np.nan
-        else:
-            fxx = (z6 - 2 * z5 + z4) / (delta_x ** 2)
-            fyy = (z2 - 2 * z5 + z8) / (delta_y ** 2)
-            fxy = (z1 - z3 - z7 + z9) / (4 * delta_x * delta_y)
-            p = fx ** 2 + fy ** 2
-            q = p + 1
-            kv = -(fxx * fx ** 2 + 2 * fxy * fx * fy + fyy * fy ** 2) / (p * (q ** 3) ** (1 / 2))
-            kh = -(fxx * fy ** 2 - 2 * fxy * fx * fy + fyy * fx ** 2) / (p * q ** (1 / 2))
-        return kv, kh
-
-
-    def courbures_approxi(self):
-        z1 = self.mnt[:-2, :-2]
-        z2 = self.mnt[:-2, 1:-1]
-        z3 = self.mnt[:-2, 2:]
-        z4 = self.mnt[1:-1, :-2]
-        z5 = self.mnt[1:-1, 1:-1]
-        z6 = self.mnt[1:-1, 2:]
-        z7 = self.mnt[2:, :-2]
-        z8 = self.mnt[2:, 1:-1]
-        z9 = self.mnt[2:, 2:]
-
-        A = (z1 + z3 + z4 + z6 + z7 + z9) / (6 * self.pas ** 2) - (z2 + z5 + z8) / (3 * self.pas ** 2)
-        B = (z1 + z2 + z3 + z7 + z8 + z9) / (6 * self.pas ** 2) - (z4 + z5 + z6) / (3 * self.pas ** 2)
-        C = (z3 + z7 - z1 - z9) / (4 * self.pas ** 2)
+    def courbures_pente_nulle(self):
+        grilleA = np.array([[1, -2, 1], [1, -2, 1], [1, -2, 1]]) / (6 * self.pas ** 2)
+        A = convolve(self.mnt, grilleA, mode='constant', cval=0.0)
+        grilleB = np.array([[1, 1, 1], [-2, -2, -2], [1, 1, 1]]) / (6 * self.pas ** 2)
+        B = convolve(self.mnt, grilleB, mode='constant', cval=0.0)
+        grilleC = np.array([[-1, 0, 1], [0, 0, 0], [1, 0, -1]]) / (4 * self.pas ** 2)
+        C = convolve(self.mnt, grilleC, mode='constant', cval=0.0)
         kmin = -A - B - np.sqrt((A - B) ** 2 + C ** 2)
         kmax = -A - B + np.sqrt((A - B) ** 2 + C ** 2)
-        kmean = (kmin + kmax) / 2
+        return kmin, kmax
 
-        return kmean
+
+    def trace_courbure(self):
+        kmin, kmax = self.courbures_pente_nulle()
+        kv, kh = self.courbures_pente_non_nulle()
+        a, b = self.mnt.shape
+        eps = 0.01
+        COULEUR = np.zeros((a, b))
+        for i in range(a):
+            for j in range(b):
+                if kv[i][j] == kh[i][j] == np.nan:
+                    if kmin[i][j] >= eps and kmax[i][j] >= eps:
+                        COULEUR[i][j] = 10
+                    elif abs(kmin[i][j]) < eps and kmax[i][j] >= eps:
+                        COULEUR[i][j] = 11
+                    elif abs(kmin[i][j]) < eps and abs(kmax[i][j]) < eps:
+                        COULEUR[i][j] = 12
+                    elif kmin[i][j] <= -eps and kmax[i][j] >= eps:
+                        COULEUR[i][j] = 13
+                    elif kmin[i][j] <= -eps and abs(kmax[i][j]) < eps:
+                        COULEUR[i][j] = 14
+                    elif kmin[i][j] <= -eps and kmax[i][j] <= -eps:
+                        COULEUR[i][j] = 15
+                else:
+                    if kv[i][j] >= eps and kh[i][j] >= eps:
+                        COULEUR[i][j] = 1
+                    elif kv[i][j] >= eps and kh[i][j] <= -eps:
+                        COULEUR[i][j] = 2
+                    elif kv[i][j] >= eps and abs(kh[i][j]) < eps:
+                        COULEUR[i][j]=3
+                    elif abs(kv[i][j]) < eps and kh[i][j] >= eps:
+                        COULEUR[i][j] = 4
+                    elif abs(kv[i][j]) < eps and abs(kh[i][j]) < eps:
+                        COULEUR[i][j] = 5
+                    elif abs(kv[i][j]) < eps and kh[i][j] <= -eps:
+                        COULEUR[i][j] = 6
+                    elif kv[i][j] <= -eps and kh[i][j] >= eps:
+                        COULEUR[i][j] = 7
+                    elif kv[i][j] <= -eps and abs(kh[i][j]) < eps:
+                        COULEUR[i][j] = 8
+                    elif kv[i][j] <= -eps and kh[i][j] <= -eps:
+                        COULEUR[i][j] = 9
+
+
+
+
+liste_couleur=[0,'blue','red','yellow','green','pink','cyan','purple','orange','magenta','black','white','gray','brown','darkgreen','darkred']
